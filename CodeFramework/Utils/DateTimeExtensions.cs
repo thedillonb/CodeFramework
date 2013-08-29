6e42348718c0b91f869c00dbb391b01ae3e963c1
Dillon Buchanan <thedillonb@gmail.com>
2013-08-29 02:56:14 -0400

    Added months and years to DateTime.ToDaysAgo

--- CodeFramework/Utils/DateTimeExtensions.cs
+++ CodeFramework/Utils/DateTimeExtensions.cs
@@ -5,6 +5,16 @@ namespace System
         public static string ToDaysAgo(this DateTime d)
         {
             var dt = DateTime.Now.Subtract(d.ToLocalTime());
+            if (dt.TotalDays >= 365)
+            {
+                var years = Convert.ToInt32(dt.TotalDays / 365); 
+                return years + (years > 1 ? " years ago".t() : " year ago".t());
+            }
+            if (dt.TotalDays >= 30)
+            {
+                var months = Convert.ToInt32(dt.TotalDays / 30); 
+                return months + (months > 1 ? " months ago".t() : " month ago".t());
+            }
             if (dt.TotalDays > 1)
                 return Convert.ToInt32(dt.TotalDays) + " days ago".t();
             if (dt.TotalHours > 1)
