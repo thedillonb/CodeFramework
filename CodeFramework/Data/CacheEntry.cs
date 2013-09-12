b915b7422c7c52d2263badc8b5e8b00a83aead70
Dillon Buchanan <thedillonb@gmail.com>
2013-09-12 00:42:52 -0400

    Added a new way to Cache using the file system

---
+++ CodeFramework/Data/CacheEntry.cs
@@ -0,0 +1,57 @@
+using System;
+using SQLite;
+
+namespace CodeFramework.Data
+{
+    public class CacheEntry
+    {
+        [AutoIncrement]
+        [PrimaryKey]
+        public int Id { get; set; }
+
+        [Indexed]
+        [MaxLength(1024)]
+        public string Query { get; set; }
+
+        [MaxLength(1024)]
+        public string Path { get; set; }
+
+        public DateTime Updated { get; set; }
+
+        [Ignore]
+        public bool IsValid
+        {
+            get
+            {
+                return System.IO.File.Exists(Path);
+            }
+        }
+
+        public T LoadResult<T>() where T : class
+        {
+            try
+            {
+                using (var io = System.IO.File.OpenRead(Path))
+                {
+                    var f = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                    return f.Deserialize(io) as T;
+                }
+            }
+            catch
+            {
+                return default(T);
+            }
+        }
+
+        public void SaveResult(object result)
+        {
+            using (var io = System.IO.File.OpenWrite(Path))
+            {
+                var f = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                f.Serialize(io, result);
+                Updated = DateTime.Now;
+            }
+        }
+    }
+}
+
---
+++ CodeFramework/Data/CacheProvider.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Collections.Generic;
+using SQLite;
+using System.Linq;
+
+namespace CodeFramework.Data
+{
+    public class CacheProvider
+    {
+        private object _lock = new object();
+        private SQLiteConnection _cacheDatabase;
+
+        public CacheProvider(SQLiteConnection cacheDatabase)
+        {
+            _cacheDatabase = cacheDatabase;
+        }
+
+        public T Get<T>(string query) where T : class
+        {
+            lock (_lock)
+            {
+                var queries = _cacheDatabase.Query<CacheEntry>("select * from CacheEntry where query = ?", query);
+                var cacheEntry = queries.FirstOrDefault();
+
+                if (cacheEntry == null)
+                    return null;
+
+                if (!cacheEntry.IsValid)
+                {
+                    //Remove it since it's not valid anymore
+                    _cacheDatabase.Delete(cacheEntry);
+                    return null;
+                }
+
+                try
+                {
+                    return cacheEntry.LoadResult<T>();
+                }
+                catch (Exception)
+                {
+                    return null;
+                }
+            }
+        }
+
+        public void Set(string query, object content)
+        {
+            lock (_lock)
+            {
+                var queries = _cacheDatabase.Query<CacheEntry>("select * from CacheEntry where query = ?", query);
+                var cacheEntry = queries.FirstOrDefault();
+                if (cacheEntry != null)
+                {
+                    cacheEntry.SaveResult(content);
+                    _cacheDatabase.Update(cacheEntry);
+                }
+                else
+                {
+                    cacheEntry = new CacheEntry { Query = query, Path = System.IO.Path.Combine(System.IO.Path.GetTempPath(), System.IO.Path.GetTempFileName()) };
+                    cacheEntry.SaveResult(content);
+                    try
+                    {
+                        if (_cacheDatabase.Insert(cacheEntry) != 1)
+                            throw new InvalidOperationException("Did not insert cache object");
+                    }
+                    catch
+                    {
+                        //Clean up the file
+                        try { System.IO.File.Delete(cacheEntry.Path); } catch { }
+                    }
+                }
+            }
+        }
+
+        public void Delete(string query)
+        {
+            lock (_lock)
+            {
+                var cacheEntry = _cacheDatabase.Table<CacheEntry>().Where(x => x.Query.Equals(query)).FirstOrDefault();
+                if (cacheEntry != null)
+                    _cacheDatabase.Delete(cacheEntry);
+            }
+        }
+
+        public void DeleteWhereStartingWith(string query)
+        {
+            lock (_lock)
+            {
+                foreach (var e in _cacheDatabase.Table<CacheEntry>().Where(x => x.Query.StartsWith(query)))
+                    _cacheDatabase.Delete(e);
+            }
+        }
+    }
+}
+
