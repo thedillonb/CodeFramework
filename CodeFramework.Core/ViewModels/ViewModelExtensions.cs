f9414d8be7abbff3332b304f5ea4d068aefbc2b2
Dillon Buchanan <thedillonb@gmail.com>
2013-11-01 23:20:20 -0400

    Removed Markup, added viewmodel extensions

---
+++ CodeFramework.Core/ViewModels/ViewModelExtensions.cs
@@ -0,0 +1,39 @@
+using System;
+using System.ComponentModel;
+using System.Collections.Specialized;
+
+public static class ViewModelExtensions
+{
+    public static void Bind<T, TR>(this T viewModel, System.Linq.Expressions.Expression<Func<T, TR>> outExpr, Action b) where T : INotifyPropertyChanged
+    {
+        var expr = (System.Linq.Expressions.MemberExpression) outExpr.Body;
+        var prop = (System.Reflection.PropertyInfo) expr.Member;
+        var name = prop.Name;
+        viewModel.PropertyChanged += (sender, e) =>
+        {
+            if (e.PropertyName.Equals(name))
+                b();
+        };
+    }
+
+    public static void Bind<T, TR>(this T viewModel, System.Linq.Expressions.Expression<Func<T, TR>> outExpr, Action<TR> b) where T : INotifyPropertyChanged
+    {
+        var expr = (System.Linq.Expressions.MemberExpression) outExpr.Body;
+        var prop = (System.Reflection.PropertyInfo) expr.Member;
+        var name = prop.Name;
+        var comp = outExpr.Compile();
+        viewModel.PropertyChanged += (sender, e) =>
+        {
+            if (e.PropertyName.Equals(name))
+                b(comp(viewModel));
+        };
+    }
+
+    public static void BindCollection<T>(this T viewModel, System.Linq.Expressions.Expression<Func<T, INotifyCollectionChanged>> outExpr, Action<NotifyCollectionChangedEventArgs> b) where T : INotifyPropertyChanged
+    {
+        var exp = outExpr.Compile();
+        var m = exp(viewModel);
+        m.CollectionChanged += (sender, e) => b(e);
+    }
+}
+
