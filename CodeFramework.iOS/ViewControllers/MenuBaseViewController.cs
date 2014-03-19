8f66592a522a1db9b49b7ef73cf8c3ed2428b998
Dillon Buchanan <thedillonb@gmail.com>
2014-03-18 23:13:14 -0400

    Revert "MenuElement images are always templated"
    This reverts commit 92e7dd0d2baafc613c399bb1bd845d041bf5698d.

--- CodeFramework.iOS/ViewControllers/MenuBaseViewController.cs
+++ CodeFramework.iOS/ViewControllers/MenuBaseViewController.cs
@@ -219,7 +219,7 @@ namespace CodeFramework.iOS.ViewControllers
                 BackgroundColor = UIColor.Clear;
                 TextColor = PrimaryColor;
                 DetailColor = UIColor.White;
-                Image = image.ImageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate);
+                Image = image;
                 Accessory = UITableViewCellAccessory.None;
             }
 
