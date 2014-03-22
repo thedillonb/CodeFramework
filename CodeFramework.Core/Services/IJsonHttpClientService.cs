ae310f04cf1ffbe5b52c25865615fdcfab046f2d
Dillon Buchanan <thedillonb@gmail.com>
2014-03-22 13:11:26 -0400

    Added JsonClient & PickerAlert

---
+++ CodeFramework.Core/Services/IJsonHttpClientService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace CodeFramework.Core.Services
+{
+    public interface IJsonHttpClientService
+    {
+        Task<TMessage> Get<TMessage>(string url);
+    }
+}
+
